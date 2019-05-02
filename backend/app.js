06565f06e12aad6d8c33ececbc52e35e4ace28fb
SrijaAthukuri <44948789+SrijaAthukuri@users.noreply.github.com>
2019-05-03 00:01:55 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app = express();
-
 app.get('/api/pokemon', function(req, res){
    res.send({
     "pokemon": [
         "bulbasaur", 
+        "charmander", 
         "squirtle"  
     ]
 });
 });
-
+app.get('/*', function(req, res){
+   res.status(404).send('Sorry!!The requested URL is not found!!!');   
+});
 app.listen(8006);
