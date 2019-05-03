62131088391317d42076b0e64d68cb99186a8c1d
SrijaAthukuri <44948789+SrijaAthukuri@users.noreply.github.com>
2019-05-03 23:57:26 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.get('/api/pokemon', function(req, res){
     ]
 });
 });
-app.get('/*', function(req, res){
+app.get('*', function(req, res){
    res.status(404).send('Sorry!!The requested URL is not found!!!');   
 });
 app.listen(8006);
